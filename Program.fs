c6b127d0b259f485d2140930cbe84487fe9dd1f7
Sergey <xxsmotur@gmail.com>
2019-02-12 23:46:13 +0500

    Add jenkins hash function

--- Program.fs
+++ Program.fs
@@ -4,9 +4,21 @@ open System
 
 let n = 10000
 
-let hash x = 
-    let h = 86813
-    List.filter x
+let jenkins (s:string) =
+    let h = 
+        System.Text.Encoding.ASCII.GetBytes(s) |> Array.fold (fun ac b ->
+            let ac = ac + (uint32 b)
+            let ac = ac + (ac <<< 10)
+            ac ^^^ (ac >>> 6)
+        ) 0u
+    let h = h + (h <<< 3)
+    let h = h ^^^ (h <<< 11)
+    h + (h <<< 15)
+
+let square x = x * x
+
+let bloom_filter n_bits n_hashes = 
+    List.Filter n_bits n_hashes
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
